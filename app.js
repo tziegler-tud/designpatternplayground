3b6e68f0f7d65ac5a54bd87288bbf56e161f3d4b
Tom Ziegler <tom.ziegler1@gmx.de>
2024-01-11 17:07:02 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+import {fileURLToPath} from "url";
+
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import bodyParser from "body-parser";
+import { create } from 'express-handlebars';
+
+
+import indexRouter from './routes/index.js';
+
+var app = express();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+global.appRoot = path.resolve(__dirname);
+
+// view engine setup
+const hbs = create({
+})
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+//TODO: Initiate Service as a singleton
+export default app;
